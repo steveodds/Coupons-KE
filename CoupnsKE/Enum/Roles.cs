29b8b812b80c471140db8febfd09f5bb00be692a
Stephen Odundo <steveremdi@gmail.com>
2020-09-05 20:13:52 +0300

    Added user roles

---
+++ CoupnsKE/Enum/Roles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CouponsKE.Enum
+{
+    public enum Roles
+    {
+        Administrator,
+        Moderator,
+        User
+    }
+}
