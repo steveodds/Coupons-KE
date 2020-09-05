a73617c62d1f484c2ca47c92c47f4961a61acdc1
Stephen Odundo <steveremdi@gmail.com>
2020-09-05 21:00:10 +0300

    Added migration for the models that were re-added

--- CoupnsKE/Data/Migrations/20200905175410_Core_Model_Addition.Designer.cs
+++ CoupnsKE/Data/Migrations/20200905175410_Core_Model_Addition.Designer.cs
@@ -4,14 +4,16 @@ using CoupnsKE.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CoupnsKE.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200905175410_Core_Model_Addition")]
+    partial class Core_Model_Addition
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
