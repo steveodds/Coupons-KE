51cc96531375e33b730d1735f69b29d2ac846fbf
Stephen Odundo <steveremdi@gmail.com>
2020-09-06 16:51:43 +0300

    Scaffolded Identity

---
+++ CoupnsKE/Areas/Identity/Data/CoupnsKEContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoupnsKE.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoupnsKE.Data
+{
+    public class CoupnsKEContext : IdentityDbContext<CoupnsKEUser>
+    {
+        public CoupnsKEContext(DbContextOptions<CoupnsKEContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ CoupnsKE/Areas/Identity/Data/CoupnsKEUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using CoupnsKE.Enum;
+
+namespace CoupnsKE.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the CoupnsKEUser class
+    public class CoupnsKEUser : IdentityUser
+    {
+        [PersonalData]
+        public string Name { get; set; }
+        [PersonalData]
+        public DateTime DOB { get; set; }
+
+        [PersonalData]
+        public Gender Gender { get; set; }
+        [PersonalData]
+        public Roles UserRole { get; set; }
+    }
+}
