0cd2fae6b13e3e4f15f30df78c7e4b6030fa1b66
Stephen Odundo <steveremdi@gmail.com>
2020-09-08 22:07:58 +0300

    Updated config method to stop timeouts

--- CoupnsKE/Startup.cs
+++ CoupnsKE/Startup.cs
@@ -12,6 +12,7 @@ using CoupnsKE.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 
 namespace CoupnsKE
 {
@@ -50,6 +51,12 @@ namespace CoupnsKE
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
