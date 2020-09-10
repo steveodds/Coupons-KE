6b4e8e4ab6bf657fdfcf06e95389836ab9353f81
Stephen Odundo <steveremdi@gmail.com>
2020-09-11 00:04:32 +0300

    Updated scaffolded coupon items

--- CoupnsKE/Controllers/CouponsController.cs
+++ CoupnsKE/Controllers/CouponsController.cs
@@ -54,7 +54,7 @@ namespace CoupnsKE.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("CouponID,StoreID,CouponCategory,CouponCode,Description,ExpiryDate,Restrictions")] Coupon coupon)
+        public async Task<IActionResult> Create([Bind("CouponID,Store,CouponCategory,CouponCode,Description,ExpiryDate,Restrictions,CouponUrl")] Coupon coupon)
         {
             if (ModelState.IsValid)
             {
@@ -87,7 +87,7 @@ namespace CoupnsKE.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(Guid id, [Bind("CouponID,StoreID,CouponCategory,CouponCode,Description,ExpiryDate,Restrictions")] Coupon coupon)
+        public async Task<IActionResult> Edit(Guid id, [Bind("CouponID,Store,CouponCategory,CouponCode,Description,ExpiryDate,Restrictions,CouponUrl")] Coupon coupon)
         {
             if (id != coupon.CouponID)
             {
