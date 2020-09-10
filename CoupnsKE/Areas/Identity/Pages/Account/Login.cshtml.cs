545aa1eab79627dc6fbf72a8fd9075ad69b8d5d5
Stephen Odundo <steveremdi@gmail.com>
2020-09-10 17:23:45 +0300

    added try catch and logged exception

--- CoupnsKE/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ CoupnsKE/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -22,7 +22,7 @@ namespace CoupnsKE.Areas.Identity.Pages.Account
         private readonly SignInManager<CoupnsKEUser> _signInManager;
         private readonly ILogger<LoginModel> _logger;
 
-        public LoginModel(SignInManager<CoupnsKEUser> signInManager, 
+        public LoginModel(SignInManager<CoupnsKEUser> signInManager,
             ILogger<LoginModel> logger,
             UserManager<CoupnsKEUser> userManager)
         {
@@ -76,35 +76,45 @@ namespace CoupnsKE.Areas.Identity.Pages.Account
         {
             //returnUrl ??= Url.Content("~/");
 
-            returnUrl ??= Url.Content("~/");
-
-            if (ModelState.IsValid)
+            try
             {
-                // This doesn't count login failures towards account lockout
-                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var checkUser = _userManager.FindByEmailAsync(Input.Email);
-                var result = await _signInManager.PasswordSignInAsync(checkUser.Result.UserName, Input.Password, Input.RememberMe, lockoutOnFailure: false);
-                if (result.Succeeded)
-                {
-                    _logger.LogInformation("User logged in.");
-                    return LocalRedirect(returnUrl);
-                }
-                if (result.RequiresTwoFactor)
-                {
-                    return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.RememberMe });
-                }
-                if (result.IsLockedOut)
-                {
-                    _logger.LogWarning("User account locked out.");
-                    return RedirectToPage("./Lockout");
-                }
-                else
-                {
-                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
-                    return Page();
-                }
-            }
+                returnUrl ??= Url.Content("~/");
 
+                if (ModelState.IsValid)
+                {
+                    // This doesn't count login failures towards account lockout
+                    // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+                    var checkUser = _userManager.FindByEmailAsync(Input.Email);
+                    var result = await _signInManager.PasswordSignInAsync(checkUser.Result.UserName, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                    if (result.Succeeded)
+                    {
+                        _logger.LogInformation("User logged in.");
+                        return LocalRedirect(returnUrl);
+                    }
+                    if (result.RequiresTwoFactor)
+                    {
+                        return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.RememberMe });
+                    }
+                    if (result.IsLockedOut)
+                    {
+                        _logger.LogWarning("User account locked out.");
+                        return RedirectToPage("./Lockout");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                        return Page();
+                    }
+                }
+
+
+            }
+            catch (global::System.Exception ex)
+            {
+
+                _logger.LogError("Failed on log in post method: " + ex.Message);
+                return Page();
+            }
             // If we got this far, something failed, redisplay form
             return Page();
         }
