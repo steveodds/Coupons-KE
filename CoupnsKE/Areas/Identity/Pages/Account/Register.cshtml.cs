6bf12be2a9be10e9730a3adba08ca4fe29ec0367
Stephen Odundo <steveremdi@gmail.com>
2020-09-12 17:51:04 +0300

    fixed issue where page required a return url

--- CoupnsKE/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ CoupnsKE/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -81,13 +81,13 @@ namespace CoupnsKE.Areas.Identity.Pages.Account
             public string ConfirmPassword { get; set; }
         }
 
-        public async Task OnGetAsync(string returnUrl = null)
+        public async Task OnGetAsync(string? returnUrl = null)
         {
             ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
         }
 
-        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        public async Task<IActionResult> OnPostAsync(string? returnUrl = null)
         {
             returnUrl = returnUrl ?? Url.Content("~/");
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
