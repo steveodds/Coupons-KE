56501bd4ecf009c99bb5b95a9d8cb472642b21f4
Stephen Odundo <steveremdi@gmail.com>
2020-09-14 00:47:21 +0300

    Added migration for the Product model modification

---
+++ CoupnsKE/Data/Migrations/20200913212818_ProductModelModification.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoupnsKE.Data.Migrations
+{
+    public partial class ProductModelModification : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StoreID",
+                table: "Product");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Product",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProductDescription",
+                table: "Product",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "StoreName",
+                table: "Product",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Product");
+
+            migrationBuilder.DropColumn(
+                name: "ProductDescription",
+                table: "Product");
+
+            migrationBuilder.DropColumn(
+                name: "StoreName",
+                table: "Product");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "StoreID",
+                table: "Product",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+    }
+}
