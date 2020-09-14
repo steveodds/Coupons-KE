55cf1fb690d4408d654b23ae7f999eb76cb27880
Stephen Odundo <steveremdi@gmail.com>
2020-09-14 23:14:18 +0300

    Migration for the Product model change (added store link)

--- CoupnsKE/Data/Migrations/20200914201204_ProductModelModificationStoreLink.Designer.cs
+++ CoupnsKE/Data/Migrations/20200914201204_ProductModelModificationStoreLink.Designer.cs
@@ -4,14 +4,16 @@ using CoupnsKE.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CoupnsKE.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200914201204_ProductModelModificationStoreLink")]
+    partial class ProductModelModificationStoreLink
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
