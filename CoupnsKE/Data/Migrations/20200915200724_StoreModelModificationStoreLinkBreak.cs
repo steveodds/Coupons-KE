6881aa2074d062e7935c50a3d14de7c74b9cd57c
Stephen Odundo <steveremdi@gmail.com>
2020-09-16 00:20:31 +0300

    Migrations for the updated store model

---
+++ CoupnsKE/Data/Migrations/20200915200724_StoreModelModificationStoreLinkBreak.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoupnsKE.Data.Migrations
+{
+    public partial class StoreModelModificationStoreLinkBreak : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StoreReflink",
+                table: "Store");
+
+            migrationBuilder.AddColumn<string>(
+                name: "StoreReflinkEnd",
+                table: "Store",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "StoreReflinkStart",
+                table: "Store",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StoreReflinkEnd",
+                table: "Store");
+
+            migrationBuilder.DropColumn(
+                name: "StoreReflinkStart",
+                table: "Store");
+
+            migrationBuilder.AddColumn<string>(
+                name: "StoreReflink",
+                table: "Store",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
