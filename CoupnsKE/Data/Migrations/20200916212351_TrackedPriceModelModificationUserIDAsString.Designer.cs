1f465ad60fe89190fb0d8f774f91434ee0bf54fb
Stephen Odundo <steveremdi@gmail.com>
2020-09-17 00:39:05 +0300

    Added migration to accomodate the product model change (UserID matches identity field)

--- CoupnsKE/Data/Migrations/20200916212351_TrackedPriceModelModificationUserIDAsString.Designer.cs
+++ CoupnsKE/Data/Migrations/20200916212351_TrackedPriceModelModificationUserIDAsString.Designer.cs
@@ -4,14 +4,16 @@ using CoupnsKE.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CoupnsKE.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200916212351_TrackedPriceModelModificationUserIDAsString")]
+    partial class TrackedPriceModelModificationUserIDAsString
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CoupnsKE/Data/Migrations/20200916212351_TrackedPriceModelModificationUserIDAsString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoupnsKE.Data.Migrations
+{
+    public partial class TrackedPriceModelModificationUserIDAsString : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserID",
+                table: "TrackedPrice",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserID",
+                table: "TrackedPrice",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+    }
+}
