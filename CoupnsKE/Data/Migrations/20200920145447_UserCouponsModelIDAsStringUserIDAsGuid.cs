965942f741c1b0772007277954cd0668c0f7ae65
Stephen Odundo <steveremdi@gmail.com>
2020-09-24 00:54:04 +0300

    Migrations for changes to UserCoupons model (UserID attribute as string to match identity)

---
+++ CoupnsKE/Data/Migrations/20200920145447_UserCouponsModelIDAsStringUserIDAsGuid.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoupnsKE.Data.Migrations
+{
+    public partial class UserCouponsModelIDAsStringUserIDAsGuid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserID",
+                table: "UserCoupons",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserID",
+                table: "UserCoupons",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+    }
+}
