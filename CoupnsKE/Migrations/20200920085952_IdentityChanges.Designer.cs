e255e32d50983ec6477c779b25934062002259ef
Stephen Odundo <steveremdi@gmail.com>
2020-09-24 00:52:15 +0300

    Added missed changes for Identity modifications with a migration

--- CoupnsKE/Migrations/20200920085952_IdentityChanges.Designer.cs
+++ CoupnsKE/Migrations/20200920085952_IdentityChanges.Designer.cs
@@ -4,14 +4,16 @@ using CoupnsKE.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CoupnsKE.Migrations
 {
     [DbContext(typeof(CoupnsKEContext))]
-    partial class CoupnsKEContextModelSnapshot : ModelSnapshot
+    [Migration("20200920085952_IdentityChanges")]
+    partial class IdentityChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
