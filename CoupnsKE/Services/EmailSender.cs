2f013b068b2f4cec81239bcaaa30d5858bfb1210
Stephen Odundo <steveremdi@gmail.com>
2021-04-10 16:45:17 +0300

    Removed hardcoded api

--- CoupnsKE/Services/EmailSender.cs
+++ CoupnsKE/Services/EmailSender.cs
@@ -19,13 +19,12 @@ namespace CoupnsKE.Services
         {
             var client = new RestClient();
             client.BaseUrl = new Uri("https://api.eu.mailgun.net/v3/mail.couponske.codes");
-            client.Authenticator = new HttpBasicAuthenticator("api", "***REMOVED***");
+            client.Authenticator = new HttpBasicAuthenticator("api", Environment.GetEnvironmentVariable("MailgunAPI"));
             var request = new RestRequest();
             request.AddParameter("domain", "mail.couponske.codes", ParameterType.UrlSegment);
             request.Resource = "https://api.eu.mailgun.net/v3/mail.couponske.codes/messages";
             request.AddParameter("from", "Steve from CouponsKE <no-reply@couponske.codes>");
             request.AddParameter("to", email);
-            //request.AddParameter("to", "admin@couponske.codes");
             request.AddParameter("subject", subject);
             request.AddParameter("text", message);
             request.Method = Method.POST;
