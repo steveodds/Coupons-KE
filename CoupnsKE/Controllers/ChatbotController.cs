527596616f8facbce0541a7d26ee39af2f192e6d
Stephen Odundo <steveremdi@gmail.com>
2021-04-14 23:01:33 +0300

    Added code for reading requests to this controller

--- CoupnsKE/Controllers/ChatbotController.cs
+++ CoupnsKE/Controllers/ChatbotController.cs
@@ -8,6 +8,8 @@ using RestSharp;
 using RestSharp.Authenticators;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
+using System.IO;
+using System.Text;
 
 namespace CoupnsKE.Controllers
 {
@@ -15,6 +17,7 @@ namespace CoupnsKE.Controllers
     [ApiController]
     public class ChatbotController : ControllerBase
     {
+        private readonly IHttpContextAccessor _contextAccessor;
 
         private class Weather
         {
@@ -23,9 +26,49 @@ namespace CoupnsKE.Controllers
             public string temperature { get; set; }
         }
 
+        public ChatbotController(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
 
         [HttpGet]
-        public ActionResult Index([FromQuery] string location)
+        public ActionResult Index()
+        {
+            var request = _contextAccessor.HttpContext.Request;
+
+            var obj = GetRequestBodyAsync(request);
+
+            throw new Exception($"req ==> [{obj}]");
+            //return Ok(obj);
+        }
+
+        private async Task<string> GetRequestBodyAsync(HttpRequest request)
+        {
+
+            string objRequestBody;
+
+            // IMPORTANT: Ensure the requestBody can be read multiple times.
+            HttpRequestRewindExtensions.EnableBuffering(request);
+
+            // IMPORTANT: Leave the body open so the next middleware can read it.
+            using (StreamReader reader = new StreamReader(
+                request.Body,
+                Encoding.UTF8,
+                detectEncodingFromByteOrderMarks: false,
+                leaveOpen: true))
+            {
+                string strRequestBody = await reader.ReadToEndAsync();
+                objRequestBody = strRequestBody;
+
+                // IMPORTANT: Reset the request body stream position so the next middleware can read it
+                request.Body.Position = 0;
+            }
+            return objRequestBody;
+        }
+
+        [HttpGet]
+        [Route("/WeatherInfo")]
+        public ActionResult WeatherInfo([FromQuery] string location)
         {
             var result = WeatherData(location);
             //result = result.Remove(0, 1);
